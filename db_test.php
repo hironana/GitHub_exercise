667cffaca1fa2a49c5a34b5e0229c76bfae4820a
hirona <hirona@gmail.com>
2014-06-02 19:48:59 +0900

    2nd Commit

--- db_test.php
+++ db_test.php
@@ -1,7 +1,7 @@
 <?php
-	$mysqli = @new mysqli('localhost','root','');
-	if ($mysqli->connect_error) {
-		die('接続失敗！<br>'. $mysqli->connect_error);
+	$link = mysqli_connect('localhost','root','');
+	if (!$link) {
+		die('接続失敗！<br>'. $mysqli_connect_error());
 	} else {
 		echo '接続成功!';
 	}
